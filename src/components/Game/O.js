6e8cb4af08591b42e992fb4210b2135046a7472d
PiyushIngale007 <ingalepiyush@gmail.com>
2021-07-02 09:57:15 +0530

    CSS for X & O

---
+++ src/components/Game/O.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+const beforeStyle = {
+  background: 'black',
+  width: '90%',
+  height: '90%',
+  position: 'absolute',
+  borderRadius: '50%',
+  left: '5px',
+  // transform: 'rotate(45deg)'
+};
+const afterStyle = {
+  background: 'white',
+  width: '70%',
+  height: '70%',
+  position: 'absolute',
+  borderRadius: '50%',
+  left: '15px',
+  top: '10px',
+  // transform: 'rotate(45deg)'
+};
+
+const O = () => {
+  return (
+    <>
+      <div className='before' style={beforeStyle}></div>
+      <div className='after' style={afterStyle}></div>
+    </>
+  );
+};
+
+export default O;
